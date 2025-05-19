45374e08107d094acc0a9bb863225c04827ab167
Kamil Akhmedkhanov <=>
2025-05-19 21:15:39 +0300

    fix

--- src/shared/components/AddressAutocomplete.vue
+++ src/shared/components/AddressAutocomplete.vue
@@ -2,39 +2,75 @@
 import { ref } from 'vue'
 import axios from 'axios'
 
-const state = ref('')
+interface IDtoItem {
+  value: string
+  data: {
+    geo_lat: string
+    geo_lon: string
+  }
+}
 
-const querySearchAsync = async (query: string, cb: (arg: any) => void) => {
+interface IItem {
+  value: string
+  coords: {
+    lat: number | null
+    lng: number | null
+  }
+}
+
+const emit = defineEmits<{
+  (name: 'select', event: IItem): void
+}>()
+
+const query = ref('')
+
+const querySearchAsync = async (query: string, cb: (arg: IItem[]) => void) => {
   const token = "58c32a9a6700723589530251df0175b4cb16a540"; // TODO env
 
   try {
-    const res = await axios.post('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address', { query: query }, {
+    const res = await axios.post<{suggestions: IDtoItem[]}>('https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address', { query: query }, {
       headers: {
         Authorization: "Token " + token
       }
     })
-    console.log('res', res.data)
+
+    const suggestions = res.data.suggestions || []
+
+    const items = suggestions.map((item): IItem => ({
+      value: item.value,
+      coords: {
+        lat: +item.data.geo_lat || null,
+        lng: +item.data.geo_lon || null
+      }
+    }))
+    cb(items)
   } catch (e) {
     console.log(e)
   }
-
-
-  setTimeout(() => {
-    cb([{ value: 'kama' }])
-  }, 3000)
 }
 
-function handleSelect(item: any) {
-  console.log(item)
+function handleSelect(item: IItem) {
+  emit('select', item)
+}
+
+function onInput() {
+  emit('select', {
+    value: query,
+    coords: {
+      lat: null,
+      lng: null,
+    }
+  })
 }
 </script>
 
 <template>
+  <h1>{{query}}</h1>
   <ElAutocomplete
-    v-model="state"
+    v-model="query"
     :fetch-suggestions="querySearchAsync"
-    placeholder="Please input"
     @select="handleSelect"
+    @input="onInput"
   >
     <!--        <template #default="{ item }">-->
     <!--          <div class="value">{{ item.value }}</div>-->
